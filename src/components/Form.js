1783c9da792c52fd2c832ddd88b8ca1b5fc2f4e1
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-09 22:38:56 -0500

    Validation and Error component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from './Error';
 
 const Form = () => {
+    const [termino, setTermino] = useState('');
+    const [error, setError] = useState(false);
+
+    // Search images
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // Validation
+        if(termino.trim() === '') {
+            setError(true);
+            return;
+        }
+        setError(false);
+
+        // Send termino to App
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen. Ej: Fútbol, Café"
+                        onChange={e => setTermino(e.target.value)}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -19,6 +38,7 @@ const Form = () => {
                     />
                 </div>
             </div>
+            { error ? <Error mensaje="Agrega un término de búsqueda" /> : null}
         </form>
     );
 }
